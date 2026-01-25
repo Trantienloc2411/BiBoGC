d827832ad90ea041da4cec3a680689796bf29aa8
Tran Tien Loc <62489239+trantienloc2411@users.noreply.github.com>
2026-01-25 22:21:38 +0700

    Add GetProductVariantsQuery.cs for get all variants of all products

---
+++ BiBoGC/InventoryManagement.Application/Queries/GetProductVariants/GetProductVariantsQuery.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManagement.Application.DTOs;
+using MediatR;
+using Shared.Application.Common;
+
+namespace InventoryManagement.Application.Queries.GetProductVariants;
+
+public class GetProductVariantsQuery : IRequest<PagedResult<ProductVariantDto>>
+{
+    public int PageSize { get; init; } = 10;
+    public int PageNumber { get; init; } = 1;
+    public int TotalCount { get; init; }
+    public string? SearchString { get; init; }
+}
